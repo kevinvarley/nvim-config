e2d5e6acc558c33d59c51c0fa0a3b37ecec8612c
Kevin Varley <kevin@swtdigital.com>
2023-02-03 10:37:16 +0000

    Set defaults, spaces for tabs, etc.

--- lua/veeyee/init.lua
+++ lua/veeyee/init.lua
@@ -3,4 +3,13 @@ require("veeyee.packer")
 
 vim.wo.number = true
 vim.opt.termguicolors = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.colorcolumn = "120"
